324bcc416bafad9eea0d18ce05fd90ab28c2765c
mariunsj <mariunsj@stud.ntnu.no>
2022-03-24 16:58:42 +0100

    Added files

--- core/src/com/mygdx/game/ColorSplash.java
+++ core/src/com/mygdx/game/ColorSplash.java
@@ -1,31 +1,33 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.States.GameStateManager;
+import com.mygdx.game.States.MenuState;
 
-public class ColorSplash extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class ColorSplash extends Game {
+
+	public static final int V_WIDTH=412;
+	public static final int V_HEIGHT=732;
+
+	public static final String TITLE = "Color Splash";
+
+	private GameStateManager gsm;
+	public SpriteBatch batch;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		super.render();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 }
