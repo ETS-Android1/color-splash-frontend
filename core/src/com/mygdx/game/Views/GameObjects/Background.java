91ec0d00f1baf88ab35daf522e466cfe425428d4
mariunsj <mariunsj@stud.ntnu.no>
2022-03-28 11:01:07 +0200

    Changed from state to view

--- core/src/com/mygdx/game/Views/GameObjects/Background.java
+++ core/src/com/mygdx/game/Views/GameObjects/Background.java
@@ -1,12 +1,14 @@
-package com.mygdx.game.Models;
+package com.mygdx.game.Views.GameObjects;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.ColorSplash;
 
 public class Background extends GameObject{
 
-    public Background(Texture image, int xPos, int yPos) {
+    public Background(Texture image, int xPos, int yPos ) {
         super(image, xPos, yPos);
+
     }
 
     public void drawBackground(SpriteBatch sb) {
--- core/src/com/mygdx/game/Views/GameObjects/GameObject.java
+++ core/src/com/mygdx/game/Views/GameObjects/GameObject.java
@@ -1,4 +1,4 @@
-package com.mygdx.game.Models;
+package com.mygdx.game.Views.GameObjects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
