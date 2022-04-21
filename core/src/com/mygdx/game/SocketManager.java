927eee221b6260f43c781f394804dcf01e659192
Carl Valdemar Ebbesen <ecvebbesen@gmail.com>
2022-04-21 13:02:55 +0200

    add events

--- core/src/com/mygdx/game/SocketManager.java
+++ core/src/com/mygdx/game/SocketManager.java
@@ -5,7 +5,15 @@ public class SocketManager {
         try {
 			socket = IO.socket("https://color-splash.herokuapp.com");
             socket.connect();
-            
+            //socket.on(EventsConstants.error, ...);
+            //socket.on(EventsConstants.gameCreated, ...);
+            //socket.on(EventsConstants.gameInfo, ...);
+            //socket.on(EventsConstants.displayColors, ...);
+            //socket.on(EventsConstants.roundStarted, ...);
+            //socket.on(EventsConstants.timesUp, ...);
+            //socket.on(EventsConstants.endRound, ...);
+            //socket.on(EventsConstants.gameFinished, ...);
+            //socket.on(EventsConstants.gameDeleted, ...);
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
